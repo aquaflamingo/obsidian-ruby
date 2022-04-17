3270e8907280314f5d4b4dc8091875f06f2d253f
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-04-17 11:43:13 -0700

    Initializes repository

---
+++ lib/obsidianmd.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "obsidianmd/version"
+
+module ObsidianMd
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/obsidianmd/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ObsidianMd
+  VERSION = "0.1.0"
+end
