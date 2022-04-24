e1a128c140ff28fedead8cdc655dfc9acd86c771
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-04-23 19:46:00 -0700

    Continue implementation of file tree

--- lib/obsidian/file_tree.rb
+++ lib/obsidian/file_tree.rb
@@ -1,31 +1,60 @@
 class FileTree
-  attr_reader :content, parent, :name
+  attr_accessor :content, parent, :name
 
-  def self.from_path(path)
-    # Start
-    # Set base path as root node
-    # Glob all dir/files in path
-    #
-    # if glob is empty 
-    #   return
-    # 
-    # For each dir/file in glob
-    #
-    # Recursion
-    #     Add dir/file to node children
-    #
-    #   Base case: Check if file is a !directory
-    #     return
-    #
-    #   Else
+  class Builder
+    # TODO make builder
+    def add(ft)
+      # Look at all files proximal to this file
+      files = Dir.glob(File.join(path, "*"))
+
+      # If there are no proximate files
+      if files.empty?
+        return
+
+      files.each do |f|
+        child_ft = FileTree.new
+        child_ft.name = File.basename(f)
+        child_ft.content = f
+
+        ft.add_child(child_ft)
+
+        add(ft)
+      end
+    end
+
+    def from_path(path)
+      ft = new 
+
+      ft.root!
+
+      add(ft)
+
+      # Start
+      # Set base path as root node
+      # Glob all dir/files in path
+      #
+      # if glob is empty 
+      #   return
+      # 
+      # For each dir/file in glob
+      #
+      # Recursion
+      #     Add dir/file to node children
+      #
+      #   Base case: Check if file is a !directory
+      #     return
+      #
+      #   Else
+    end
   end
+
 
   def initialize
     @children = []
     @children_hash = {}
   end
 
-  def add(child)
+  def add_child(child)
     raise ArgumentError.new "child cannot be nil" unless child
 
     if @children_hash.keys.include?(child.name)
@@ -37,6 +66,10 @@ class FileTree
     @children_hash[child.name] = child
     @children << child
     child
+  end
+
+  def root!
+    @parent = nil
   end
 
   def root?
