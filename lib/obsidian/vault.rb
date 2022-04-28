538dae527d940e4f3d2de629e63693eb69ce3b79
aquaflamingo <16901597+aquaflamingo@users.noreply.github.com>
2022-04-27 19:13:33 -0700

    Add FileManager and other not-yet-implemented methods

--- lib/obsidian/vault.rb
+++ lib/obsidian/vault.rb
@@ -1,18 +1,22 @@
+require_relative "file_tree"
+
 # 
 # Vault is a representation of an Obsidian.md Vault Object
 #
 class Vault
-  def initialize(config)
+  attr_reader :name
+
+  def initialize(config, name)
     @config = config
-
-    read_files
+    @name = name
+    @fm = FileManager.new(@config.root_path)
   end
 
-  def self.load(path)
-    new(Config.new(path, File.basename))
+  def self.load(path, name)
+    new(Config.new(path), name)
   end
 
-  def new_folder
+  def new_dir
     raise NotImplementedError
   end
 
@@ -20,13 +24,43 @@ class Vault
     raise NotImplementedError
   end
 
-  private 
-
+  # 
+  # Config represents a vaults configuration values for operating
+  #
   class Config
-    attr_reader :path, :name
-    def initialize(path, name)
-      @path = path
-      @name = name
+    attr_reader :root_path, :config_path
+
+    DEFAULT_CONFIG_LOCATION = '.obsidian'.freeze
+
+    # 
+    # For now, simply always assume default configuration file path
+    #
+    def initialize(path, config_path = DEFAULT_CONFIG_LOCATION)
+      @config_path = File.join path, config_path
+
+      @root_path = path
+    end
+  end
+
+  # 
+  # FileManager is an interface for accessing the in-memory
+  # file tree of a vault.
+  #
+  class FileManager
+    def initialize(root)
+      @ft = FileTree::Builder.from_path(root)
+    end
+
+    def ls
+      raise NotImplementedError
+    end
+
+    def new_file(file_name, at)
+      raise NotImplementedError
+    end
+
+    def new_dir(dir_name, at)
+      raise NotImplementedError
     end
   end
 end
